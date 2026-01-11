2597091313c5d6e10f8c5bc5e791078ba1e3ca1d
trinhhieu <hieu.trinh@ntq-solution.com.vn>
2026-01-11 11:22:43 +0700

    sdga

--- Haha.java
+++ Haha.java
@@ -1,16 +1,5 @@
 public class Haha {
     public static void main(String[] args) {
-        
-        System.out.println("1 ahahah");
-        if (true) {
-
-        }
-        System.out.println("dgads");
-        System.out.println("HieuTQ");
-
-        System.out.println("kilogam");
-        System.out.println("sdga");
-        System.out.println("dsga");
-        System.out.println("dga");
+        System.out.println("1");
     }
 }
